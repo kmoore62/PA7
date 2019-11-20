c5eebb84db7cff4098cf3e9eea0ec099c586bb6f
kmoore62 <57967579+kmoore62@users.noreply.github.com>
2019-11-20 12:02:07 -0600

    Compact.cpp

---
+++ Compact.cpp
@@ -0,0 +1,12 @@
+#include "Compact.h"
+#include <iostream>
+
+using namespace std;
+
+Compact::Compact(){
+//declare an array of size capacity
+}
+
+Compact::~Compact(){
+//delete the dynamic memoory
+}
