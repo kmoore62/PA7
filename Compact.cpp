66afaa75e677ed408e6b1a5196cafa8b7e325e81
kmoore62 <57967579+kmoore62@users.noreply.github.com>
2019-11-20 14:54:34 -0600

    Update Compact.cpp

--- Compact.cpp
+++ Compact.cpp
@@ -1,12 +1,40 @@
 #include "Compact.h"
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 Compact::Compact(){
 //declare an array of size capacity
+  parray= new int[capcity];
+  tos=-1;
 }
 
 Compact::~Compact(){
-//delete the dynamic memoory
+//delete the dynamic memory
+  delete []parray;
 }
+
+bool Compact::empty()const{
+  return tos==-1;
+}
+
+bool Compact::full() const{
+  return tos == capacity-1;
+}
+
+void Compact::push(string name){
+  //throw exception if it is full
+  ++tos;
+  parray[tos]= name;
+}
+
+string Compact::top() const{
+  return parray[tos];
+}
+
+void Compact::pop(){
+  --tos;
+}
+
+
