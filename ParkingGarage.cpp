b9cc86beb1a591d76e197774430776a5db32fcac
kmoore62 <57967579+kmoore62@users.noreply.github.com>
2019-11-20 11:51:36 -0600

    ParkingGarage.cpp

---
+++ ParkingGarage.cpp
@@ -0,0 +1,36 @@
+#include "ParkingGarage.h"
+#include <iostream>
+
+using namespace std;
+
+ParkingGarage::ParkingGarage(){
+
+}
+
+
+ParkingGarage::ParkingGarage(int capacity){
+//set the capacity
+}
+
+
+ParkingGarage::~ParkingGarage(){
+//delete dynamic memory
+}
+
+void ParkingGarage::push(Vehicle* ptr){
+
+}
+
+
+void ParkingGarage::pop(CarType){
+
+}
+
+void ParkingGarage::isVacant(CarType){
+//should this be a void function?
+}
+
+void ParkingGarage:: checkStatus(){
+//should this be void?
+}
+
