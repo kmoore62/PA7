09dc0f6b367b021cd85ce15980b863e6c1ca70bb
kmoore62 <57967579+kmoore62@users.noreply.github.com>
2019-11-20 15:13:12 -0600

    Update Truck.h

--- Truck.h
+++ Truck.h
@@ -2,9 +2,13 @@
 
 class Truck: protected vehicle{
 protected:
-
+  node* ptop;
 public:
   Truck();
   ~Truck();
-
+  void push(string);
+  void pop();
+  string top() const;
+  bool empty() const;
+  bool full() const;
 };
