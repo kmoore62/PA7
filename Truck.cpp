3e6969b3d8a843d7c8b7db46924a577c83e0ea54
kmoore62 <57967579+kmoore62@users.noreply.github.com>
2019-11-20 15:26:51 -0600

    Update Truck.cpp

--- Truck.cpp
+++ Truck.cpp
@@ -1,9 +1,40 @@
 #include "Truck.h"
+#include <iostream>
+using namespace std;
 
-Truck::Truck(){
+Truck::Truck(): ptop(nullptr){
 //initialize a singly linked list of size capacity
 }
 
 Truck::~Truck(){
 //delete dynamic memory
+}
+
+void Truck::push(string name){
+
+}
+
+void Truck:: pop(){
+  if(ptop->link==nullptr){
+    //this is for only one node
+    delete ptop;
+    ptop=nullptr;
+  }
+  else{
+    node* discard= ptop;
+    ptop= ptop->link;
+    delete discard;
+  }
+}
+
+bool Truck::empty() const{
+  return ptop==nullptr;
+}
+
+bool Truck::full() const{
+  return false;
+}
+
+String Truck::top() const{
+  return ptop->name;
 }
