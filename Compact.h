417092f171d429ce2a71736d54b4f8c292e19d55
kmoore62 <57967579+kmoore62@users.noreply.github.com>
2019-11-20 14:39:22 -0600

    Update Compact.h

--- Compact.h
+++ Compact.h
@@ -1,9 +1,19 @@
+#pragma once
 #include "vehicle.h"
+#include <string>
 
 class Compact: protected vehicle{
 protected:
-
+  int* parray;
+  //int size; //capacity which is defined in the vehicle
+  int tos; //this is the top of stack
 public:
-Compact();
-~Compact();
-}
+  Compact();
+  Compact(int);
+  ~Compact();
+  bool empty() const;
+  bool full() const;
+  void push(string);
+  //string top() const;
+  void pop();
+};
