554bcecf53c3f8ea56ed98b67d5a7cc415db099f
klykins123 <58006377+klykins123@users.noreply.github.com>
2019-11-23 16:33:09 -0600

    Update ParkingGarage.cpp

--- ParkingGarage.cpp
+++ ParkingGarage.cpp
@@ -3,71 +3,65 @@
 #include "Compact.h"
 #include <iostream>
 
+
+
 using namespace std;
 
-ParkingGarage::ParkingGarage(){
+ParkingGarage::ParkingGarage() {
 
 }
 
 
-ParkingGarage::ParkingGarage(int capacity) : size(s), tos(-1){
+ParkingGarage::ParkingGarage(int capacity) : size(capacity), compacttos(-1), trucktos(-1) {
 	compactArray = new int[capacity];
 	truckArray = new int[capacity];
 }
 
 
-ParkingGarage::~ParkingGarage(){
-//delete dynamic memory
+ParkingGarage::~ParkingGarage() {
+	//delete dynamic memory
 }
 
-void ParkingGarage::push(Vehicle* ptr){
-	if(ptr == Compact){
-	++tos;
-	compactArray[tos] = ptr;
+void ParkingGarage::push(vehicle* ptr) {
+	if (ptr == Compact) {
+		++compacttos;
+		compactArray[compacttos] = ptr;
 	}
-	
-	if(ptr == Truck){
-	++tos;
-	truckArray[tos] = ptr;
+
+	if (ptr == Truck) {
+		++trucktos;
+		truckArray[trucktos] = ptr;
 	}
 }
 
 
-void ParkingGarage::pop(CarType x){
-	if(x == COMPACT){
+void ParkingGarage::pop(CarType x) {
+	if (x == COMPACT) {
 		compacttos--;
 	}
 
-	if(x == TRUCK){
+	if (x == TRUCK) {
 		trucktos--;
 	}
 }
 
-void ParkingGarage::isVacant(CarType x){
-	if(x == COMPACT){
-		if(compacttos == 10){
-			return "Full";
-		} else {
-			return "Vacant";
-		}
+void ParkingGarage::isVacant(CarType x) {
+	if (x == COMPACT) {
+		//run Compact empty and full exceptions
 	}
 
-	if(x == TRUCK){
-		if(trucktos == 10){
-			return "Full";
-		} else {
-			return "Vacant";
-		}
+	if (x == TRUCK) {
+		//run Truck empty and full exceptions
 	}
 }
 
-void ParkingGarage:: checkStatus(){
-  while (true){
-  
-   cout << "------------------------------------------------" << endl;
-	cout << "|     Lane 1    |    Lane 2     |    Lane 3    |" << endl;
-	cout << "|---------------|---------------|--------------|" << endl;
-  cout << "|
-  }
-//should this be void?
+void ParkingGarage::checkStatus() {
+	while (true) {
+
+		cout << "------------------------------------------------" << endl;
+		cout << "|     Lane 1    |    Lane 2     |    Lane 3    |" << endl;
+		cout << "|---------------|---------------|--------------|" << endl;
+		cout << "|
+	}
+	//should this be void?
 }
